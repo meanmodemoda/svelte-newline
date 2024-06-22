f6791a9dc828cd12b211b5381de83c4c91690389
Yang Zhao <muonius@icloud.com>
2024-06-22 16:05:20 +0800

    tooltip

--- simple-scatterplot/src/App.svelte
+++ simple-scatterplot/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import AxisX from "$components/AxisX.svelte";
   import AxisY from "$components/AxisY.svelte";
+  import Tooltip from "$components/Tooltip.svelte";
   import data from "$data/data.js";
 
   import { scaleLinear } from "d3-scale";
@@ -17,30 +18,43 @@
     .range([0, innerWidth]);
   const innerHeight = height - margin.top - margin.bottom;
 
-
-
   let yScale = scaleLinear()
     .domain([0, max(data, d => d.hours)])
     .range([innerHeight, 0]);
+  let hoveredData;
+$: console.log(hoveredData)
+ 
+ 
 </script>
 <div class="chart-container" bind:clientWidth={width}>
 
-<svg {width} {height}>
+<svg {width} {height}
+on:mouseleave={() =>hoveredData=null}>
   <g class='inner-chart' transform="translate({margin.left}, {margin.top})">
     <AxisY width={innerWidth} {yScale} />
     <AxisX height={innerHeight} width={innerWidth} {xScale} />
-    {#each data as d}
+    <!-- sorting data for tab -->
+    {#each data.sort((a,b)=>a.grade-b.grade) as d}
       <circle
         cx={xScale(d.grade)}
         cy={yScale(d.hours)}
-        r={10}
+        r={hoveredData === d ? 20:10}
+        opacity={hoveredData? (hoveredData === d ? 1:0.65) : 1}
         fill="purple"
         stroke="black"
         stroke-width={1}
+        on:mouseover={() =>hoveredData=d}
+        on:focus={() =>hoveredData=d}
+     tabindex="0"
+
+      
       />
     {/each}
   </g>
 </svg>
+{#if hoveredData}
+<Tooltip data={hoveredData} {xScale} {yScale} width={innerWidth} />
+{/if}
 </div>
 
 <style>
@@ -48,5 +62,8 @@
     font-weight: 400; /* How thick our text is */
     font-size: 12px; /* How big our text is */
     fill: hsla(212, 10%, 53%, 1); /* The color of our text */
+  }
+  circle {
+    transition: r 300ms, opacity 500ms ease;
   }
 </style>
