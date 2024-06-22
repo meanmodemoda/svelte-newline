20bb576c1b188da82d874bbecb753e09eb64da03
Yang Zhao <muonius@icloud.com>
2024-06-22 12:24:02 +0800

    responsive

--- simple-scatterplot/src/App.svelte
+++ simple-scatterplot/src/App.svelte
@@ -1,30 +1,52 @@
 <script>
+  import AxisX from "$components/AxisX.svelte";
+  import AxisY from "$components/AxisY.svelte";
   import data from "$data/data.js";
 
   import { scaleLinear } from "d3-scale";
   import { max } from "d3-array";
 
   let width = 400;
+  $: console.log(width)
   let height = 400;
 
-  let xScale = scaleLinear()
+  const margin = { top: 20, right: 15, bottom: 20, left: 0 };
+  $: innerWidth = width - margin.right - margin.left;
+  $: xScale = scaleLinear()
     .domain([0, 100])
-    .range([0, width]);
+    .range([0, innerWidth]);
+  const innerHeight = height - margin.top - margin.bottom;
+
+
 
   let yScale = scaleLinear()
     .domain([0, max(data, d => d.hours)])
-    .range([height, 0]);
+    .range([innerHeight, 0]);
 </script>
+<div class="chart-container" bind:clientWidth={width}>
 
 <svg {width} {height}>
-  {#each data as d}
-    <circle
-      cx={xScale(d.grade)}
-      cy={yScale(d.hours)}
-      r={10}
-      fill="purple"
-      stroke="black"
-      stroke-width={1}
-    />
-  {/each}
+  <g class='inner-chart' transform="translate({margin.left}, {margin.top})">
+    <AxisY width={innerWidth} {yScale} />
+    <AxisX height={innerHeight} width={innerWidth} {xScale} />
+    {#each data as d}
+      <circle
+        cx={xScale(d.grade)}
+        cy={yScale(d.hours)}
+        r={10}
+        fill="purple"
+        stroke="black"
+        stroke-width={1}
+      />
+    {/each}
+  </g>
 </svg>
+</div>
+
+<style>
+  :global(.tick text, .axis-title) {
+    font-weight: 400; /* How thick our text is */
+    font-size: 12px; /* How big our text is */
+    fill: hsla(212, 10%, 53%, 1); /* The color of our text */
+  }
+</style>
